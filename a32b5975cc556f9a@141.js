1782ee749b78f7153f4f8730b388886649a64984
anton-eris-zheltoukhov <30631037+anton-eris-zheltoukhov@users.noreply.github.com>
2020-03-19 15:12:57 +0300

    clean up test

--- a32b5975cc556f9a@141.js
+++ a32b5975cc556f9a@141.js
@@ -200,14 +200,18 @@ parseNewick(await FileAttachment("life.txt").text())
   main.variable(observer("outerRadius")).define("outerRadius", ["width"], function(width){return(
 width / 2
 )});
-  main.variable(observer("innerRadius")).define("innerRadius", ["outerRadius"], function(outerRadius){return(
-outerRadius - 170
-)});
+
+//   main.variable(observer("innerRadius")).define("innerRadius", ["outerRadius"], function(outerRadius){return(
+// outerRadius - 170
+// )});
+
   main.variable(observer("parseNewick")).define("parseNewick", function(){return(
 function parseNewick(a){for(var e=[],r={},s=a.split(/\s*(;|\(|\)|,|:)\s*/),t=0;t<s.length;t++){var n=s[t];switch(n){case"(":var c={};r.branchset=[c],e.push(r),r=c;break;case",":var c={};e[e.length-1].branchset.push(c),r=c;break;case")":r=e.pop();break;case":":break;default:var h=s[t-1];")"==h||"("==h||","==h?r.name=n:":"==h&&(r.length=parseFloat(n))}}return r}
 )});
+
   main.variable(observer("d3")).define("d3", ["require"], function(require){return(
 require("d3@5")
 )});
+
   return main;
 }
