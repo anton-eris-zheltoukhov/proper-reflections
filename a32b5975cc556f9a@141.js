34df15bd044c599f7fa044e85043eee52959a5f5
anton-eris-zheltoukhov <30631037+anton-eris-zheltoukhov@users.noreply.github.com>
2020-03-19 20:18:00 +0300

    Revert "try to rotate chart (2)"
    This reverts commit df139078c60e251b81ab4a02bf72997c4b48f643.

--- a32b5975cc556f9a@141.js
+++ a32b5975cc556f9a@141.js
@@ -87,7 +87,17 @@ export default function define(runtime, observer) {
     link.transition(t).attr("d", checked ? linkVariable : linkConstant);
   }
 
-
+function animateTriangle(){
+    svg.select("g")
+        .transition()
+        .duration(2500)
+        .attr('transform' , rotate(-180))
+        
+        .transition() //And rotate back again
+        .duration(2500)
+        .attr('transform' , rotate(-180))        
+        .on("end", animateTriangle) ;  //at end, call it again to create infinite loop
+}
 
   function mouseovered(active) {
     return function(d) {
@@ -218,18 +228,6 @@ export default function define(runtime, observer) {
   main.variable(observer("d3")).define("d3", ["require"], function(require){return(
   require("d3@5")
   )});
-
-  function animateTriangle(){
-    svg.select("g")
-        .transition()
-        .duration(2500)
-        .attr('transform' , rotate(-180))
-        
-        .transition() //And rotate back again
-        .duration(2500)
-        .attr('transform' , rotate(-180))        
-        .on("end", animateTriangle) ;  //at end, call it again to create infinite loop
-}
 
   animateTriangle()
 
