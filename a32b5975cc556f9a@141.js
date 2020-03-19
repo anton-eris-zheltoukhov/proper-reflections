5e902e856f16aabc4f5351878bb26c1bf70273cd
anton-eris-zheltoukhov <30631037+anton-eris-zheltoukhov@users.noreply.github.com>
2020-03-19 20:18:04 +0300

    Revert "try to rotate (1)"
    This reverts commit b04731dd41478d5e0c3c874f931209482d64d760.

--- a32b5975cc556f9a@141.js
+++ a32b5975cc556f9a@141.js
@@ -87,18 +87,6 @@ export default function define(runtime, observer) {
     link.transition(t).attr("d", checked ? linkVariable : linkConstant);
   }
 
-function animateTriangle(){
-    svg.select("g")
-        .transition()
-        .duration(2500)
-        .attr('transform' , rotate(-180))
-        
-        .transition() //And rotate back again
-        .duration(2500)
-        .attr('transform' , rotate(-180))        
-        .on("end", animateTriangle) ;  //at end, call it again to create infinite loop
-}
-
   function mouseovered(active) {
     return function(d) {
       d3.select(this).classed("label--active", active);
@@ -228,8 +216,5 @@ function animateTriangle(){
   main.variable(observer("d3")).define("d3", ["require"], function(require){return(
   require("d3@5")
   )});
-
-  animateTriangle()
-
   return main;
 }
